e4c51451fbba96ba9a25fcd4e92a110c3d1de768
iczerwinski <igorjczerwinski@gmail.com>
2016-04-07 19:49:18 +0200

    aaa

---
+++ src/main/java/com/demo/repository/CityRepository.java
@@ -0,0 +1,16 @@
+package com.demo.repository;
+
+import com.demo.entity.City;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CityRepository extends CrudRepository<City, Integer> {
+
+
+    List<City> findAll();
+
+
+}
---
+++ src/main/java/com/demo/service/CityService.java
@@ -0,0 +1,22 @@
+package com.demo.service;
+
+import com.demo.entity.City;
+import com.demo.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    CityRepository cityRepository;
+
+
+    public List<City> getCities() {
+        return cityRepository.findAll();
+    }
+
+
+}
